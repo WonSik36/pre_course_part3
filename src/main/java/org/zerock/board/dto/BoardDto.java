300849699cb95aa2bf4603fb8b2c0d9fd2e4a1e5
WonSik36 <30713548+WonSik36@users.noreply.github.com>
2021-01-20 15:16:05 +0900

    Register and Read Function added to Service

---
+++ src/main/java/org/zerock/board/dto/BoardDto.java
@@ -0,0 +1,28 @@
+package org.zerock.board.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardDto {
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+    private String writerEmail;
+
+    private String writerName;
+
+    private LocalDateTime regDate;
+
+    private LocalDateTime modDate;
+
+    private int replyCount;
+}
